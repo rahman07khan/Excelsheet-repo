84128ead7d5bbee557877557dd2693985e4ed608
rahmankhan07 <abdulrahumankhan20012177@gmail.com>
2025-03-31 17:34:49 +0530

    [MOD Updated Task 5]

--- Task 5/test1.py
+++ Task 5/test1.py
@@ -1,229 +1,8 @@
-# import re
-# import os
-
-# def qnm_extract_data(folder_path):
-#     all_results = {}
-    
-#     for filename in os.listdir(folder_path):
-#         file_path = os.path.join(folder_path, filename)
-        
-#         if os.path.isfile(file_path):
-#             print(f"\nProcessing file: {filename}")
-#             result_list = []
-
-#             if "qnm" in filename.lower():
-#                 print("Processing QNM file:", filename)
-#                 master_name = None
-                
-#                 with open(file_path, 'r') as file:
-#                     for line in file:
-#                         if '<request_type=request_handshake' in line:
-#                             master_match = re.search(r'master=([^,]+)', line)
-#                             master_name = master_match.group(1) if master_match else None
-                            
-#                             data_dict = {}
-                            
-#                             # Extract urgency (qos)
-#                             urgency_match = re.search(r'urgency=([^,]+)', line)
-#                             if urgency_match:
-#                                 data_dict["Urgency"] = urgency_match.group(1)
-                            
-#                             # Extract len (+1)
-#                             len_match = re.search(r'len=([^,]+)', line)
-#                             if len_match:
-#                                 len_value = len_match.group(1)
-#                                 try:
-#                                     len_value = str(int(len_value) + 1)
-#                                 except ValueError:
-#                                     pass
-#                                 data_dict["Len"] = len_value
-                            
-#                             # Extract trans_type
-#                             trans_type_match = re.search(r'trans_type=([^,]+)', line)
-#                             if trans_type_match:
-#                                 data_dict["Transtype"] = trans_type_match.group(1)
-                            
-#                             if master_name:
-#                                 data_dict["Master"] = master_name
-                            
-#                             result_list.append(data_dict)
-                
-#                 if result_list:
-#                     all_results[filename] = result_list
-    
-#     return all_results
-
-# def alm_extract_data(folder_path):
-#     all_results = {}
-    
-#     for filename in os.listdir(folder_path):
-#         file_path = os.path.join(folder_path, filename)
-        
-#         if os.path.isfile(file_path):
-#             print(f"\nProcessing file: {filename}")
-#             result_list = []
-
-#             if "alm" in filename.lower():
-#                 print("Processing ALM file:", filename)
-#                 master_name = None
-                
-#                 with open(file_path, 'r') as file:
-#                     for line in file:
-#                         if '<request_type=request_handshake' in line:
-#                             master_match = re.search(r'master=([^,]+)', line)
-#                             master_name = master_match.group(1) if master_match else None
-                            
-#                             data_dict = {}
-                            
-#                             # Extract urgency (arqos)
-#                             urgency_match = re.search(r'arqos=([^,>\s]+)', line)
-#                             if urgency_match:
-#                                 data_dict["Urgency"] = urgency_match.group(1)
-                            
-#                             # Extract len (arlen)
-#                             len_match = re.search(r'arlen=([^,>\s]+)', line)
-#                             if len_match:
-#                                 len_value = len_match.group(1)
-#                                 data_dict["Len"] = len_value
-                            
-#                             # Extract trans_type
-#                             trans_type_match = re.search(r'trans_type=([^,]+)', line)
-#                             if trans_type_match:
-#                                 data_dict["Transtype"] = trans_type_match.group(1)
-                            
-#                             if master_name:
-#                                 data_dict["Master"] = master_name
-                            
-#                             result_list.append(data_dict)
-                
-#                 if result_list:
-#                     all_results[filename] = result_list
-    
-#     return all_results
-
-# def chm_extract_data(folder_path):
-#     all_results = {}
-    
-#     for filename in os.listdir(folder_path):
-#         file_path = os.path.join(folder_path, filename)
-        
-#         if os.path.isfile(file_path):
-#             print(f"\nProcessing file: {filename}")
-#             result_list = []
-
-#             if "chm" in filename.lower():
-#                 print("Processing CHM file:", filename)
-#                 master_name = None
-                
-#                 with open(file_path, 'r') as file:
-#                     for line in file:
-#                         if '<request_type=request_handshake' in line:
-#                             master_match = re.search(r'master=([^,]+)', line)
-#                             master_name = master_match.group(1) if master_match else None
-                            
-#                             data_dict = {}
-                            
-#                             # Extract urgency (rEQFLIT_QOS)
-#                             urgency_match = re.search(r'rEQFLIT_QOS:\'h([0-9a-fA-F]+)', line)
-#                             if urgency_match:
-#                                 data_dict["Urgency"] = urgency_match.group(1)
-                            
-#                             # Extract len (rEQFLIT_SIZE)
-#                             len_match = re.search(r'rEQFLIT_SIZE:\'h([0-9a-fA-F]+)', line)
-#                             if len_match:
-#                                 len_value = len_match.group(1)
-#                                 data_dict["Len"] = len_value
-                            
-#                             # Extract trans_type
-#                             trans_type_match = re.search(r'trans_type=([^,]+)', line)
-#                             if trans_type_match:
-#                                 data_dict["Transtype"] = trans_type_match.group(1)
-                            
-#                             if master_name:
-#                                 data_dict["Master"] = master_name
-                            
-#                             result_list.append(data_dict)
-                
-#                 if result_list:
-#                     all_results[filename] = result_list
-    
-#     return all_results
-
-# def qns_extract_data(folder_path):
-#     all_results = {}
-    
-#     for filename in os.listdir(folder_path):
-#         file_path = os.path.join(folder_path, filename)
-        
-#         if os.path.isfile(file_path):
-#             print(f"\nProcessing file: {filename}")
-#             result_list = []
-
-#             if "qns" in filename.lower():
-#                 print("Processing QNS file:", filename)
-#                 master_name = None
-                
-#                 with open(file_path, 'r') as file:
-#                     for line in file:
-#                         if '<request_type=request_handshake' in line:
-#                             master_match = re.search(r'master=([^,]+)', line)
-#                             master_name = master_match.group(1) if master_match else None
-                            
-#                             data_dict = {}
-                            
-#                             # Extract urgency (qos)
-#                             urgency_match = re.search(r'urgency=([^,]+)', line)
-#                             if urgency_match:
-#                                 data_dict["Urgency"] = urgency_match.group(1)
-                            
-#                             # Extract len (+1)
-#                             len_match = re.search(r'len=([^,]+)', line)
-#                             if len_match:
-#                                 len_value = len_match.group(1)
-#                                 try:
-#                                     len_value = str(int(len_value) + 1)
-#                                 except ValueError:
-#                                     pass
-#                                 data_dict["Len"] = len_value
-                            
-#                             # Extract trans_type
-#                             trans_type_match = re.search(r'trans_type=([^,]+)', line)
-#                             if trans_type_match:
-#                                 data_dict["Transtype"] = trans_type_match.group(1)
-                            
-#                             if master_name:
-#                                 data_dict["Master"] = master_name
-                            
-#                             result_list.append(data_dict)
-                
-#                 if result_list:
-#                     all_results[filename] = result_list
-    
-#     return all_results
-
-# folder_path = r"E:\Main Folder\Task 5"
-
-# # Process all file types
-# qnm_extracted_data = qnm_extract_data(folder_path)
-# alm_extracted_data = alm_extract_data(folder_path)
-# chm_extracted_data = chm_extract_data(folder_path)
-# qns_extracted_data = qns_extract_data(folder_path)
-
-# # Combine all results if needed
-# all_extracted_data = {
-#     "qnm": qnm_extracted_data,
-#     "alm": alm_extracted_data,
-#     "chm": chm_extracted_data,
-#     "qns": qns_extracted_data
-# }
-
-# print("\nFinal combined results:")
-# print(all_extracted_data)
-
-
 import re
 import os
 from collections import defaultdict
+import pandas as pd
+
 
 def qnm_extract_data(folder_path):
     all_results = defaultdict(list)
@@ -294,9 +73,10 @@ def alm_extract_data(folder_path):
                             data_dict["Urgency"] = urgency_match.group(1)
                         
                         # Extract len (arlen)
-                        len_match = re.search(r'arlen=([^,>\s]+)', line)
+                        len_match = re.search(r'arsize=([^,>\s]+)', line)
                         if len_match:
-                            data_dict["Len"] = len_match.group(1)
+                            arbyte_size = len_match.group(1)
+                            data_dict["Len"] = str(2 ** len(arbyte_size) )
                         
                         # Extract trans_type
                         trans_type_match = re.search(r'trans_type=([^,]+)', line)
@@ -340,7 +120,11 @@ def chm_extract_data(folder_path):
                         # Extract trans_type
                         trans_type_match = re.search(r'trans_type=([^,]+)', line)
                         if trans_type_match:
-                            data_dict["Transtype"] = trans_type_match.group(1)
+                            transtype =  trans_type_match.group(1)
+                            if transtype in ["CHI_RSP_OPC_COMP","CHI_REQ_OPC_WRITENOSNPFULL"]:
+                                data_dict["Transtype"] = "WRITE"
+                            else:
+                                data_dict["Transtype"] = "READ"
                         
                         data_dict["Source"] = filename
                         all_results[master_name].append(data_dict)
@@ -391,32 +175,125 @@ def qns_extract_data(folder_path):
     
     return all_results
 
-def save_results_to_files(results, output_folder, prefix):
+# def save_results_to_files(results, output_folder):
+#     if not os.path.exists(output_folder):
+#         os.makedirs(output_folder)
+    
+#     for master_name, entries in results.items():
+#         # Create a safe filename
+#         safe_master_name = re.sub(r'[\\/*?:"<>|]', "_", master_name)
+#         filename = f"{safe_master_name}.txt"
+#         filepath = os.path.join(output_folder, filename)
+#         urgency_wise_total = {}
+#         transtype_dict = {"READ": 0, "WRITE": 0}
+    
+#         with open(filepath, 'w') as f:
+#             for entry in entries:
+#                 urgency = entry["Urgency"]
+#                 length = entry["Len"]  # Avoid using 'len' as it is a built-in function
+#                 transtype = entry["Transtype"]
+                
+#                 transtype_dict[transtype] += 1
+                
+#                 if urgency not in urgency_wise_total:
+#                     urgency_wise_total[urgency] = {}
+                
+#                 if length not in urgency_wise_total[urgency]:
+#                     urgency_wise_total[urgency][length] = {"READ": 0, "WRITE": 0}
+                
+#                 urgency_wise_total[urgency][length][transtype] += 1
+                
+#                 f.write(f"Urgency: {urgency}, Len: {length}, Transtype: {transtype}\n")
+#             f.write("Urgency Wise data\n")
+#             print("urgency_wise_total",urgency_wise_total,"safe_master_name",safe_master_name)
+#             for urgency, length_data in urgency_wise_total.items():
+#                 f.write(f"Urgency: {urgency}\n")
+#                 for length, val in length_data.items():
+#                     f.write(f"Len: {length}, READ: {val['READ']}, WRITE: {val['WRITE']}\n")
+
+# # Main execution
+# folder_path = os.path.dirname(os.path.abspath(__file__))
+# output_folder = os.path.join(folder_path, "Extracted_Results")
+
+# # Process all file types
+# qnm_results = qnm_extract_data(folder_path)
+# alm_results = alm_extract_data(folder_path)
+# chm_results = chm_extract_data(folder_path)
+# qns_results = qns_extract_data(folder_path)
+
+# # Save results to files
+# save_results_to_files(qnm_results, output_folder)
+# save_results_to_files(alm_results, output_folder)
+# save_results_to_files(chm_results, output_folder)
+# save_results_to_files(qns_results, output_folder)
+
+# print("\nExtraction complete. Results saved to:", output_folder)
+
+
+
+def save_results_to_excel(results, output_folder):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     
+    excel_path = os.path.join(output_folder, "Extracted_Results.xlsx")
+    writer = pd.ExcelWriter(excel_path, engine='xlsxwriter')
+    
+    # Prepare main summary sheet data
+    main_data = []
+    master_sheets = {}
+    
     for master_name, entries in results.items():
-        # Create a safe filename
-        safe_master_name = re.sub(r'[\\/*?:"<>|]', "_", master_name)
-        filename = f"{prefix}_{safe_master_name}.txt"
-        filepath = os.path.join(output_folder, filename)
+        # Create a safe sheet name
+        safe_master_name = re.sub(r'[\/*?:"<>|]', "_", master_name)
+        urgency_wise_total = {}
+        transtype_dict = {"READ": 0, "WRITE": 0}
         
-        with open(filepath, 'w') as f:
-            f.write(f"Master: {master_name}\n")
-            f.write("="*50 + "\n")
+        master_data = []
+        
+        for entry in entries:
+            urgency = entry["Urgency"]
+            length = entry["Len"]
+            transtype = entry["Transtype"]
             
-            for i, entry in enumerate(entries, 1):
-                f.write(f"Entry {i}:\n")
-                f.write(f"  Source: {entry.get('Source', 'N/A')}\n")
-                f.write(f"  Urgency: {entry.get('Urgency', 'N/A')}\n")
-                f.write(f"  Len: {entry.get('Len', 'N/A')}\n")
-                f.write(f"  Transtype: {entry.get('Transtype', 'N/A')}\n")
-                f.write("-"*40 + "\n")
+            transtype_dict[transtype] += 1
             
-            f.write(f"\nTotal entries: {len(entries)}\n")
+            if urgency not in urgency_wise_total:
+                urgency_wise_total[urgency] = {}
+            
+            if length not in urgency_wise_total[urgency]:
+                urgency_wise_total[urgency][length] = {"READ": 0, "WRITE": 0}
+            
+            urgency_wise_total[urgency][length][transtype] += 1
+            
+            master_data.append({"Urgency": urgency, "Len": length, "Transtype": transtype})
+        
+        # Store master data in a dictionary to be written later
+        master_sheets[safe_master_name] = pd.DataFrame(master_data)
+        
+        # Add data to the main summary sheet
+        for urgency, length_data in urgency_wise_total.items():
+            for length, val in length_data.items():
+                main_data.append({
+                    "Master": safe_master_name,
+                    "Urgency": urgency,
+                    "Len": length,
+                    "READ": val['READ'],
+                    "WRITE": val['WRITE']
+                })
+    
+    # Save main summary sheet first
+    df_main = pd.DataFrame(main_data)
+    df_main.to_excel(writer, sheet_name="Main", index=False)
+    
+    # Save each master sheet
+    for sheet_name, df in master_sheets.items():
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+    
+    writer.close()
+    print("Data successfully saved to", excel_path)
 
 # Main execution
-folder_path = r"E:\Main Folder\Task 5"
+folder_path = os.path.dirname(os.path.abspath(__file__))
 output_folder = os.path.join(folder_path, "Extracted_Results")
 
 # Process all file types
@@ -425,10 +302,10 @@ alm_results = alm_extract_data(folder_path)
 chm_results = chm_extract_data(folder_path)
 qns_results = qns_extract_data(folder_path)
 
-# Save results to files
-save_results_to_files(qnm_results, output_folder, "QNM")
-save_results_to_files(alm_results, output_folder, "ALM")
-save_results_to_files(chm_results, output_folder, "CHM")
-save_results_to_files(qns_results, output_folder, "QNS")
+# Combine all results
+all_results = {**qnm_results, **alm_results, **chm_results, **qns_results}
+
+# Save results to an Excel file
+save_results_to_excel(all_results, output_folder)
 
 print("\nExtraction complete. Results saved to:", output_folder)
